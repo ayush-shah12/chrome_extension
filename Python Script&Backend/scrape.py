833ba37a9a6e815b576af1aefa11489216eab457
Ayush S <ayushsh285@gmail.com>
2024-07-25 20:18:09 -0400

    fixed data format + samples

--- Python Script&Backend/scrape.py
+++ Python Script&Backend/scrape.py
@@ -1,8 +1,9 @@
 import asyncio
 from playwright.async_api import async_playwright
+from nameToCode import nameToCode
 
 
-async def main():
+async def main(url, prof_name):
     async with async_playwright() as p:
 
         responses = []
@@ -24,12 +25,37 @@ async def main():
 
         await page.route("**/graphql", handle_route)
 
-        await page.goto('https://www.ratemyprofessors.com/search/professors/971?q=tripathi')
+        await page.goto('https://www.ratemyprofessors.com/search/professors/' + url + '?q=' + prof_name)
 
         await browser.close()
 
         return responses
 
 
-data = asyncio.run(main())
-print(data)
+def set_url():
+    # add backend method here
+    school_name = "Stony Brook University (SUNY)"
+    return str(nameToCode.get(school_name))
+
+
+data = asyncio.run(main(set_url(), "lopez"))
+
+list_prof = data[0]['data']['search']['teachers']['edges']
+
+# iterate like this:
+# for prof in list_prof:
+#     print(prof['node'])
+#     
+
+# sample data 
+# | | | |
+# V V V V
+#
+# {'__typename': 'Teacher', 'avgDifficulty': 2.6, 'avgRating': 4.8, 'department': 'Physics', 'firstName':
+# 'Glenn', 'id': 'VGVhY2hlci04MTIxMjA=', 'isSaved': False, 'lastName': 'Lopez', 'legacyId': 812120, 'numRatings': 5, 
+# 'school': {'id': 'U2Nob29sLTk3MQ==', 'name': 'Stony Brook University (SUNY)'}, 'wouldTakeAgainPercent': 100}  
+#
+# {'__typename': 'Teacher', 'avgDifficulty': 2.8, 'avgRating': 4.4, 'department': 'Languages', 'firstName': 
+# 'Victoriano', 'id': 'VGVhY2hlci0zMzU4NjI=', 'isSaved': False, 'lastName': 'Roncero Lopez', 'legacyId': 335862, 
+# 'numRatings': 13, 'school': {'id': 'U2Nob29sLTk3MQ==', 'name': 'Stony Brook University (SUNY)'}, 
+# 'wouldTakeAgainPercent': 66.6667}
