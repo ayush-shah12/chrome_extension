8e7a14f8e6468e3a6ca7a89eac0def865f8b511b
AYUSH <ayushsh285@gmail.om>
2024-07-31 20:09:34 -0400

    update port

--- backend.py
+++ backend.py
@@ -84,5 +84,5 @@ def get_professor_info():
 
 
 if __name__ == '__main__':
-    port = 8080
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)       
