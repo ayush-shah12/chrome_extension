98b94df8a36c4197aeeefc5591315f9113d90453
AYUSH <ayushsh285@gmail.om>
2024-07-31 21:52:11 -0400

    refactor

--- backend.py
+++ backend.py
@@ -11,33 +11,53 @@ CORS(app)
 chrome_binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chromium_executable_path = os.environ.get("CHROMEDRIVER_PATH")
 
+browser = None
+context = None
+
+def setup_browser():
+    global browser, context
+    if browser is None:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(
+                executable_path=chromium_executable_path,
+                headless=True,
+                args=[
+                    f'--binary={chrome_binary_location}',
+                    '--no-sandbox',
+                    '--disable-gpu',
+                    '--disable-web-security',
+                    '--disable-dev-shm-usage'
+                ]
+            )
+            context = browser.new_context()
+
+def close_browser():
+    global browser
+    if browser:
+        browser.close()
+        browser = None
+
 
 def main(url, prof_name):
-    with sync_playwright() as p:
+    global context
+    responses = []
+    page = context.new_page()
 
-        responses = []
-        browser = p.chromium.launch(executable_path=chromium_executable_path,headless=True, proxy=None,
-                                          args=[f'--binary={chrome_binary_location}','--no-sandbox', '--headless', '--disable-gpu', '--disable-web-security',
-                                                '--disable-dev-sherlock', '--disable-infobars', '--disable-extensions',
-                                                '--disable-dev-tools'])
-        context = browser.new_context()
-        page = context.new_page()
+    def handle_route(route, request):
+        if "graphql" in request.url and request.method == "POST":
+            response = route.fetch()
+            json_data = response.json()
+            responses.append(json_data)
+        else:
+            route.continue_()
 
-        def handle_route(route, request):
-            if "graphql" in request.url and request.method == "POST":
-                response = route.fetch()
-                json_data = response.json()
-                responses.append(json_data)
-            else:
-                route.continue_()
+    page.route("**/graphql", handle_route)
 
-        page.route("**/graphql", handle_route)
+    page.goto('https://www.ratemyprofessors.com/search/professors/' + str(url) + '?q=' + prof_name)
 
-        page.goto('https://www.ratemyprofessors.com/search/professors/' + str(url) + '?q=' + prof_name)
+    page.close()
 
-        browser.close()
-
-        return responses
+    return responses
 
 
 def set_url(school_name):
@@ -57,9 +77,6 @@ def get_professor_info():
     prof_first_name = request.args.get('prof_first_name').lower()
     prof_last_name = request.args.get('prof_last_name')
     school_name = request.args.get('school_name')
-
-    if(prof_first_name == "tripathi"):
-        return jsonify({'MESSAGE': "This is a test message"})
 
     if set_url(school_name) is None:
         return jsonify({'ERROR': 651, 'MESSAGE': "COULD NOT FIND SCHOOL, BAD SCHOOL NAME", })
@@ -86,4 +103,8 @@ def get_professor_info():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port) 
+    try:
+        setup_browser()
+        app.run(host='0.0.0.0', port=port) 
+    finally:
+        close_browser()
