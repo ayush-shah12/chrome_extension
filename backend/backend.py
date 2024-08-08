ae80de6793d5bc81256988a13fc0263d02bdb652
Kamil <76923760+KamilWoskowiak@users.noreply.github.com>
2024-08-08 02:02:56 -0400

    Updated Backend
    Backend now grabs tags, and also grabs extra data alongside the comments including the date, course, and if the student would take it again.

--- backend/backend.py
+++ backend/backend.py
@@ -123,22 +123,56 @@ def get_data(school_code, prof_first, prof_last):
     return data
 
 
-def gen_comment(code, state):
-    if not state:  # To disable comments, switch state to false in gen_comment calls.
-        return "Comments were disabled."
+def student_input(code):
+
+    num = 15 # MAX NUMBER OF COMMENTS ALLOWED
 
     url = "https://www.ratemyprofessors.com/professor/" + str(code)
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-    comments = []
-    comments_html = soup.find_all('div', {'class': 'Comments__StyledComments-dzzyvm-0 gRjWel'})
-    comments = [div.get_text(strip=True) for div in comments_html]
+    tags_html = soup.find_all('div', {'class': 'TeacherTags__TagsContainer-sc-16vmh1y-0 dbxJaW'})
+    tags = []
+    for tag_div in tags_html:
+        spans = tag_div.find_all('span', class_='Tag-bs9vf4-0 hHOVKF')
+        for span in spans:
+            tags.append(span.get_text(strip=True))
 
-    if not comments:
-        return ['No Comments Available']
-    else:
-        return comments[slice(6)]
+    cards = soup.find_all(class_='Rating__RatingInfo-sc-1rhvpxz-3 kEVEoU')
+    user_cards = []
+    for card in cards[:num]:
+        card_data = {}
+        course = card.find('div', class_='RatingHeader__StyledClass-sc-1dlkqw1-3 eXfReS')
+        if course:
+            card_data['course'] = course.get_text(strip=True)
+        else:
+            card_data['course'] = "N/A"
+
+        date = card.find('div', class_='TimeStamp__StyledTimeStamp-sc-9q2r30-0 bXQmMr RatingHeader__RatingTimeStamp-sc-1dlkqw1-4 iwwYJD')
+        if date:
+            card_data['date'] = date.get_text(strip=True)
+        else:
+            card_data['date'] = "N/A"
+
+        comment = card.find('div', class_='Comments__StyledComments-dzzyvm-0 gRjWel')
+        if comment:
+            card_data['comment'] = comment.get_text(strip=True)
+        else:
+            card_data['comment'] = "N/A"
+
+        wta_list = card.find_all('div', class_='MetaItem__StyledMetaItem-y0ixml-0 LXClX')
+        for wta in wta_list:
+            span = wta.find('span')
+            if 'Would Take Again' in wta.get_text() and span:
+                card_data['wta'] = span.get_text(strip=True)
+        if 'wta' not in card_data:
+            card_data['wta'] = "N/A"
+        user_cards.append(card_data)
+
+    return {
+        'tags': tags,
+        'userCards': user_cards
+    }
 
 
 @app.route('/get_professor_info', methods=['GET'])
@@ -167,18 +201,23 @@ def get_professor_info():
 
         # Check if we get an exact match for the first name and last name
         if prof.get('firstName').lower() == prof_first_name and prof.get('lastName').lower() == prof_last_name and prof.get('school').get('id') == str(school_code):
-            prof['comments'] = gen_comment(prof.get('legacyId'), True)
+            student_in = student_input(prof.get('legacyId'))
+            prof['tags'] = student_in['tags']
+            prof['userCards'] = student_in['userCards']
             return jsonify(prof)
         
         # matching first name OR last name ONLY if above clause didn't trigger
         if prof.get('firstName').lower() == prof_first_name or prof.get('lastName').lower() == prof_last_name and prof.get('school').get('id') == str(school_code):
-            prof['comments'] = gen_comment(prof.get('legacyId'), True)
+            student_in = student_input(prof.get('legacyId'))
+            prof['tags'] = student_in['tags']
+            prof['userCards'] = student_in['userCards']
             return jsonify(prof)
 
         # Check in the instance that the first name was abbreviated to only the first letter
-        if len(prof_first_name) == 1 and prof.get('firstName').lower()[0] == prof_first_name[0] and prof.get(
-                'lastName').lower() == prof_last_name and prof.get('school').get('id') == str(school_code):
-            prof['comments'] = gen_comment(prof.get('legacyId'), True)
+        if len(prof_first_name) == 1 and prof.get('firstName').lower()[0] == prof_first_name[0] and prof.get('lastName').lower() == prof_last_name and prof.get('school').get('id') == str(school_code):
+            student_in = student_input(prof.get('legacyId'))
+            prof['tags'] = student_in['tags']
+            prof['userCards'] = student_in['userCards']
             return jsonify(prof)
 
     # Otherwise return error.
